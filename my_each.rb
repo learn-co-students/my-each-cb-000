c3da91bce981103b78271dface6d94bfbc41e143
John Hillegass <cyrodil@Dragonborn.local>
2017-02-20 10:35:21 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection) # put argument(s) here
+  array = collection.to_a
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+  array
 end
