afcf7492196d3921d10e495c02d5e54a5457079c
Joe Corwin <joe.corwin@gmail.com>
2017-08-18 23:58:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+
+arg = [1, 2, 3, 4]
+def my_each(arg)
+  i = 0
+  while i < arg.length
+    yield(arg[i])
+    i = i+1
+  end
+  arg
 end
+
+my_each(arg) do |number|
+    print number
+  end
