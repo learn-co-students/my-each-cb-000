c7023a1094379390542fed945632ad7bbc0f4c44
Mohammad Khan <sparrowangels@gmail.com>
2017-09-04 04:47:50 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array) # put argument(s) here
+  #try to write your own version of the #each method without using the #each method provided by Ruby.
+  #accept an argument of an array
+  #use the while loop to iterate over each member of that array, yielding each element contained in the array to a block.
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i+=1
+      # binding.pry
+    end # while
+    array
+  else
+    # console.log "Hey! No block given!"
+  end #if block_given?
 end
+
+ 
